16a5e3a9e25d296ed1550719f37911ad337c2498
Julio Garcia <cggjulio@gmail.com>
2017-02-14 14:34:37 -0600

    implemeting de edit and update action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:edit, :update]
 
 	def index
 		@portfolio_items = Portfolio.all
@@ -17,7 +18,22 @@ class PortfoliosController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @portfolio_item.update(portfolio_params)
+			redirect_to portfolios_path
+		else
+			render :edit
+		end
+	end
+
 	private
+
+	def set_portfolio_item
+		@portfolio_item = Portfolio.find(params[:id])
+	end
 
 	def portfolio_params
 		params.require(:portfolio).permit(:title, :subtitle, :body)
