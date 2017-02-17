7d53b888f4b12621bd8b3a0351aad1076fb0cd01
Julio Garcia <cggjulio@gmail.com>
2017-02-16 21:43:19 -0600

    custom routes portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :portfolios
+  resources :portfolios, except: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # aqui aparte de pasarle una url que nosotros quisimos con as: le pasamos por string el prefix del path que queremos que tome esa url
 
   get 'about-me', to: 'pages#about' # personalizamos el route para solo tener que poner /about en vez de pages/about
 
