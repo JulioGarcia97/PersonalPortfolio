a73b5edd8e3e54e8a66acaa9a0922a342fbdb378
Julio Garcia <cggjulio@gmail.com>
2017-02-21 00:42:15 -0600

    Implemented data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,18 @@ class Portfolio < ApplicationRecord
 
 	# Query para buscar por subtitulo usando scope, hacen lo mismo los dos
 	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+	# defaults instead of been in the migration
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= 'http://placehold.it/600x400'
+		self.thumb_image ||= 'http://placehold.it/300x200'
+	end
+	
+	# es lo mismo pero en una sola linea 
+
+	# if self.main_image == nil
+			# self.main_image = 'http://placehold.it/600x400'
+	# end
 end
