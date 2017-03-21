9902793daea7bf6a0e9b4e29ede33a17b14c336e
Julio Garcia <cggjulio@gmail.com>
2017-03-20 19:20:55 -0600

    Implemented view helper for login links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def login_helper
+	 if current_user.is_a?(User)
+			link_to 'Cerrar Sesion', destroy_user_session_path, method: :delete
+	 else
+			(link_to 'Iniciar Sesion', new_user_session_path) + 
+			'<br>'.html_safe +
+			(link_to 'Crear Cuenta', new_user_registration_path)
+	 end
+	end
+
 end
