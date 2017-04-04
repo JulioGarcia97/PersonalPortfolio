04bfa31868aea966c9e56a5b74df1a3628d1baa4
Julio Garcia <cggjulio@gmail.com>
2017-04-03 23:38:28 -0500

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,45 @@ module ApplicationHelper
 	def copyright_helper
 		@copyright = JgarciaViewTool::RenderCopyRight.copyright('JGarcia', 'Todos los derechos reservados')
 	end
+
+	def nav_items
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},
+			{
+				url: about_me_path,
+				title: 'Acerca de mi'
+			},
+			{
+				url: contact_path,
+				title: 'Contacto'
+			},
+			{
+				url: blogs_path,
+				title: 'Blog'
+			},
+			{
+				url: portfolios_path,
+				title: 'Portafolio'
+			},
+		]
+	end
+
+	def nav_helper(style, tag_type)
+		nav_links = ''
+
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'</a>#{item[:title]}</#{tag_type}>"
+
+		end
+
+		nav_links.html_safe
+
+	end
+
+	def active?(path)
+		"active" if current_page? path
+	end
 end
